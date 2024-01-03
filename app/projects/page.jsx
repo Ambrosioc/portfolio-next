83fcf22e00368d186e5c9340ceb62ff8a3cae3e7
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2024-01-03 03:50:41 +0100

    finish projets page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+	{
+		image: "/work/1.png",
+		category: "NextJs",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/2.png",
+		category: "ReactJs",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/3.png",
+		category: "FullStack",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/4.png",
+		category: "Symfony",
+		name: "Nexa Website",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/1.png",
+		category: "NextJs",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/2.png",
+		category: "ReactJs",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/3.png",
+		category: "FullStack",
+		name: "Nexa Website",
+		description: "Lorem ipsum dolor sit amet, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+	{
+		image: "/work/4.png",
+		category: "Symfony",
+		name: "Nexa Website",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, elit arcu.",
+		link: "/",
+		github: "/",
+	},
+];
+
+const uniqueCategories = [
+	"Tous les projets",
+	...new Set(projectData.map((project) => project.category)),
+];
 
 const Projects = () => {
-	return <div>Projects Page</div>;
+	const [categories, setCategories] = useState(uniqueCategories);
+	const [category, setCategory] = useState("Tous les projets");
+
+	const filteredProjects = projectData.filter((project) => {
+		return category === "Tous les projets"
+			? project
+			: project.category === category;
+	});
+
+	return (
+		<section className="min-h-screen pt-12">
+			<div className="container mx-auto">
+				<h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+					Mes Projets
+				</h2>
+				{/* tabs */}
+				<Tabs defaultValue={category} className="mb-24 xl:mb-48">
+					<TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none ">
+						{categories.map((category, index) => (
+							<TabsTrigger
+								onClick={() => setCategory(category)}
+								value={category}
+								key={index}
+								className="capitalize w-[162px] md:w-auto">
+								{category}
+							</TabsTrigger>
+						))}
+					</TabsList>
+					{/* tabs content */}
+					<div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+						{filteredProjects.map((project, index) => {
+							return (
+								<TabsContent key={index} value={category}>
+									<ProjectCard project={project} />
+								</TabsContent>
+							);
+						})}
+					</div>
+				</Tabs>
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
