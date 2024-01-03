e88d4199c6c5cd918725483f9fc8e2da048b644a
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2024-01-03 03:08:07 +0100

    finish home page

--- components/layout/footer/Footer.jsx
+++ components/layout/footer/Footer.jsx
@@ -1,5 +1,25 @@
+import Socials from "@/components/Socials";
+
 const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className="bg-secondary py-12">
+			<div className="container mx-auto">
+				<div className="flex flex-col items-center justify-center">
+					{/* social */}
+					<Socials
+						containerStyles={"flex gap-x-6 mx-auto mb-4 xl:mx-0 "}
+						iconsStyles={
+							"text-primary text-[20px] dark:text-white/70 hover:text-white dark:hover:text-primary transition-all"
+						}
+					/>
+					{/* copyright */}
+					<div className="text-muted-foreground">
+						Copyright &copy; Ambrosie Cazimira. All rights reserved.
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
